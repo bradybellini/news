dfb76bf8ec6e54dbc9c2bcf3cd97f4da48ec26c0
Brady Bellini <bbellini@pm.me>
2022-01-30 00:35:07 -0800

    Update testing.py

--- testing.py
+++ testing.py
@@ -4,4 +4,4 @@ import pprint
 r = RSSParser()
 pp = pprint.PrettyPrinter(indent=2)
 
-pp.pprint(r.articles('https://kotaku.com/rss')[1])
+pp.pprint(r.articles('https://www.vg247.com/feed', '123')[1])
