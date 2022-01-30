af14b95793263997896d3ac5e86c5893bfaaa986
Brady Bellini <bbellini@pm.me>
2022-01-30 00:35:04 -0800

    added feed_id
    added the feed id to smooth out the lookup and temporarily got rid of author and prob permanently got rid of tags

--- rssparser.py
+++ rssparser.py
@@ -1,3 +1,4 @@
+
 import feedparser
 import hashlib
 
@@ -7,22 +8,20 @@ class RSSParser:
         pass
 
     # double check author and tags if they return a list
-    def _get_article_info(self, feed: str, article: dict) -> dict:
-        # tags = getattr(article, "tags", "none")
-        authors = getattr(article, "authors", "no authors listed")
-        guid = article.link if article.guidislink else article.guid
+    def _get_article_info(self, feed_id: str, article: dict) -> dict:
+        authors = getattr(article, "author", "no authors listed")
         return {
+            "feed_id": feed_id,
             "title": article.title,
             "link": article.link,
-            "author": authors,
-            # "tags": tags,
+            # "author": authors,
             "published": article.published,
-            "summary": article.summary,
-            # "guid": guid,
-            # "uid": hashlib.md5(str(article.title + article.link).encode()).hexdigest(),
+            "summary": getattr(article, "summary", "none"),
+            "guid": article.link if article.guidislink else article.guid,
+            "uid": hashlib.md5(str(article.title + article.link).encode()).hexdigest(),
         }
     
-    def articles(self, feed):
+    def articles(self, feed, feed_id):
         parsed = feedparser.parse(feed)
         articles = [parsed for parsed in parsed.entries]
-        return [self._get_article_info(parsed.feed, entry) for entry in articles]
+        return [self._get_article_info(feed_id, entry) for entry in articles]
