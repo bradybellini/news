e764c0f5be7cb3d35b118fcf5f2585ef9347601b
Brady Bellini <bbellini@pm.me>
2022-01-29 17:06:30 -0800

    testing flask

--- app.py
+++ app.py
@@ -1,7 +1,29 @@
-from flask import Flask
+from encodings import utf_8
+import string
+from webbrowser import get
+import feedparser, json, hashlib
+from supabase_test import insert_articles
+from flask import Flask, jsonify, request
+
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "hello"
+
+    
+@app.route("/api/", methods=["POST"])
+def get_feed():
+    feed = request.args.get("feed", -1, type=str)
+    # insert_articles(d)
+    # return hashlib.md5(feed.encode()).hexdigest()
+    return feed
+    
+    # d = feedparser.parse(feed)
+    # c = []
+
+
+# # print(d.entries[1]['title'])
+    # for i in range(len(d.entries)):
+    #      c.append(d.entries[i].title)
+    # return jsonify(c)
+    # return jsonify([entries for entries in d.entries])
