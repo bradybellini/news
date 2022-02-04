bfbcdd6190c7057d2862e7ba00813092877d2a59
Brady Bellini <bbellini@pm.me>
2022-02-03 21:47:46 -0800

    Create supainsert.py

---
+++ supainsert.py
@@ -0,0 +1,20 @@
+import os
+from dotenv import load_dotenv
+from supainsert import create_client, Client
+from rssparser import RSSParser
+
+
+class SupaInsert:
+    def __init__(self, table: str) -> None:
+        load_dotenv()
+        self.url: str = os.environ.get("SUPABASE_URL")
+        self.key: str = os.environ.get("SUPABASE_KEY")
+        self.table = table
+
+    def _create_client(self) -> Client:
+        supabase: Client = create_client(self.url, self.key)
+        return supabase
+
+    def insert(self, feed: str, feed_id: str):
+        r = RSSParser(feed, feed_id)
+
