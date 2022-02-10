995f108dae9cd1cdda91cb48b0452838ee155e81
Brady Bellini <bbellini@pm.me>
2022-02-10 14:01:17 -0800

    Update rssparser.py

--- rssparser.py
+++ rssparser.py
@@ -2,6 +2,7 @@ from feedparser import parse
 from lxml import html
 from lxml.html.clean import clean_html
 import hashlib
+import time
 
 
 class RSSParser:
@@ -9,16 +10,18 @@ class RSSParser:
         pass
 
     def _get_article_info(self, feed_id: str, article: dict) -> dict:
-        authors = getattr(article, "author", "no authors listed")
-        summary = getattr(article, "summary", "none")
-        if summary != "none":
+        summary = getattr(article, "summary", "None")
+        # print(article["published_parsed"])
+        date = time.strftime('%Y-%m-%d %H:%M:%S', article["published_parsed"])
+        print(date)
+        if summary != "None":
             summary = clean_html(html.fromstring(summary)).text_content().strip()
 
         return {
             "feed_id": feed_id,
             "title": article.title,
             "link": article.link,
-            "author": authors,
+            "author": getattr(article, "author", "No Authors Listed"),
             "published": article.published,
             "summary": summary,
             "guid": article.link if article.guidislink else article.guid,
