2d50cd254eb8697b03eb95eb0856dc579d304e07
Brady Bellini <bbellini@pm.me>
2022-02-10 14:01:18 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
 from supapsql import SupaPSQL
+from rssparser import RSSParser
 
-supa = SupaPSQL()
+
+r = RSSParser()
+articles = r.articles("https://www.gameinformer.com/rss.xml", "b52da7d9-1a18-4ce3-af41-546f6568f127")
+
+# print(articles)
+# supa = SupaPSQL()
 
 # insert = supai.insert("https://www.gameinformer.com/rss.xml", "b52da7d9-1a18-4ce3-af41-546f6568f127")
-supa.run()
+# supa.run()
 
 # print(insert)
 
