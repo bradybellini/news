037ca81af49d442ac15ee9545b1253e3c988c379
Brady Bellini <bbellini@pm.me>
2022-02-17 23:11:06 -0800

    Update test.py

--- test.py
+++ test.py
@@ -5,6 +5,8 @@ from rssparser import RSSParser
 r = RSSParser()
 articles = r.articles("https://www.gameinformer.com/rss.xml", "b52da7d9-1a18-4ce3-af41-546f6568f127")
 
+# articles = r.articles("https://www.newyorker.com/feed/everything", "d30d7c83-0ebc-44b7-8935-012d4125e5c7")
+
 # print(articles)
 # supa = SupaPSQL()
 
