9e8bec746543abccdc663a639f1983a81d660d7b
Brady Bellini <bbellini@pm.me>
2022-02-18 21:26:01 -0800

    Update test.py

--- test.py
+++ test.py
@@ -2,16 +2,16 @@ from supapsql import SupaPSQL
 from rssparser import RSSParser
 
 
-r = RSSParser()
-articles = r.articles("https://www.gameinformer.com/rss.xml", "b52da7d9-1a18-4ce3-af41-546f6568f127")
+# r = RSSParser()
+# articles = r.articles("https://www.macworld.com/feed", "9228b3b2-970a-4f34-b88b-53c05e163d8b")
+
+# articles = r.articles("https://www.huffpost.com/section/front-page/feed", "42d45f8b-491b-432c-88bf-a78d9ae1b87b")
 
 # articles = r.articles("https://www.newyorker.com/feed/everything", "d30d7c83-0ebc-44b7-8935-012d4125e5c7")
 
 # print(articles)
-# supa = SupaPSQL()
 
-# insert = supai.insert("https://www.gameinformer.com/rss.xml", "b52da7d9-1a18-4ce3-af41-546f6568f127")
-# supa.run()
+supa = SupaPSQL()
+supa.run()
 
-# print(insert)
 
