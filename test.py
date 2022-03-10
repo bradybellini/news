ede7af7ed7b739a5f93cd5fea419006fe255e0c5
Brady Bellini <bbellini@pm.me>
2022-03-09 20:35:27 -0800

    Update test.py
    testing spacy

--- test.py
+++ test.py
@@ -1,6 +1,14 @@
 from supapsql import SupaPSQL
 from rssparser import RSSParser
 from filter import ArticleFilter
+import spacy
+from spacy import displacy
+
+ner = spacy.load("en_core_web_trf")
+raw_text="The Indian Space Research Organisation or is the national space agency of India, headquartered in Bengaluru. It operates under Department of Space which is directly overseen by the Prime Minister of India while Chairman of ISRO acts as executive of DOS as well."
+text1= ner(raw_text)
+for word in text1.ents:
+    print(word.text,word.label_)
 
 
 # list1 = ["Ad", "sponsor"]
@@ -9,29 +17,17 @@ from filter import ArticleFilter
 
 # check =  any(item in list1 for item in list2)
 # print(check)
-r = RSSParser()
+# r = RSSParser()
 # articles = r.parse_feed("https://kotaku.com/rss", "f70bd78d-71b0-4fae-9d15-9b321eb0fd75")
-articles = r.parse_feed("https://www.huffpost.com/section/front-page/feed", "42d45f8b-491b-432c-88bf-a78d9ae1b87b")
+# articles = r.parse_feed("https://www.huffpost.com/section/front-page/feed", "42d45f8b-491b-432c-88bf-a78d9ae1b87b")
 
-a = ArticleFilter()
-new_a = a.filter(articles)
-print(len(new_a))
+# a = ArticleFilter()
+# new_a = a.filter(articles)
+# print(len(new_a))
 # print(articles[5])
 # articles = r.articles("https://www.huffpost.com/section/front-page/feed", "42d45f8b-491b-432c-88bf-a78d9ae1b87b")
 
 # articles = r.articles("https://www.newyorker.com/feed/everything", "d30d7c83-0ebc-44b7-8935-012d4125e5c7")
 
-# print(articles)
-# print(articles.remove())
-
-# for i in range(len(articles)):
-# #     print(articles[i])
-#     # print(i)
-#     if "elden" in articles[i]['title'].casefold():
-#         articles.pop(i)
-#     print(articles[i]['title'])
-
-# supa = SupaPSQL()
-# supa.run()
 
 
